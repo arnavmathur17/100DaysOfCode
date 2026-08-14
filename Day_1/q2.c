d290dc675cc4fc2f871f9adbc936c0b550801530
arnavmathur17 <mathurarnav430@gmail.com>
2026-08-14 15:16:56 +0530

    Implement division by zero check in calculation
    Added error handling for division by zero.

--- Day_1/q2.c
+++ Day_1/q2.c
@@ -2,15 +2,19 @@
 
 #include <stdio.h>
 int main(){
-	int a;
-	printf("Enter first number:");
-	scanf("%d", &a);
-	int b;
-	printf("Enter second number:");
-	scanf("%d", &b);
-	printf("Sum of the two numbers is:%d\n", a+b);
-	printf("Difference of the two numbers is:%d\n", a-b);
-	printf("Product of the two numbers is:%d\n", a*b);
-	printf("Qoutient of the two numbers is: %d\n", a/b);
-	return 0;
+        int a;
+        printf("Enter first number:");
+        scanf("%d", &a);
+        int b;
+        printf("Enter second number:");
+        scanf("%d", &b);
+        printf("Sum of the two numbers is:%d\n", a+b);
+        printf("Difference of the two numbers is:%d\n", a-b);
+        printf("Product of the two numbers is:%d\n", a*b);
+        printf("Qoutient of the two numbers is: %d\n", a/b);
+        if(b==0){
+        printf("Error: denominator can't be 0 for division");
+        return 1;
+}
+        return 0;
 }
